7de4573efe322d63be744dd8a5fcd8710bba0cdf
thanyarat65 <thanyaratw65@nu.ac.th>
2024-10-11 21:02:50 +0700

    Add files via upload

---
+++ lib/models/cafe.dart
@@ -0,0 +1,18 @@
+class Cafe {
+  final int? keyID;
+  final String name;
+  final String menu;
+  final String details;
+  final String feeling;
+  final String imagePath;
+
+  Cafe({
+    this.keyID,
+    required this.name,
+    required this.menu,
+    required this.details,
+    required this.feeling,
+    required this.imagePath,
+  });
+}
+
---
+++ lib/models/record.dart
@@ -0,0 +1,17 @@
+// class Record {
+//   final String name;
+//   final String menu;
+//   final String details;
+//   final String feeling;
+//   final String imagePath;
+//   final dynamic key; // ปรับประเภทของ key ตามที่คุณต้องการ
+
+//   Record({
+//     required this.name,
+//     required this.menu,
+//     required this.details,
+//     required this.feeling,
+//     required this.imagePath,
+//     required this.key,
+//   });
+// }
